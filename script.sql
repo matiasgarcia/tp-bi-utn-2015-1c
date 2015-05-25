a913d416e517748a531a868a1165275d42c580a2
Matias Garcia <mati92.ss@gmail.com>
2015-05-25 16:00:10 -0300

    Fix truncation of decimal

--- script.sql
+++ script.sql
@@ -1,6 +1,6 @@
 USE [MIS_GRUPO_03]
 GO
-/******  Object:  Table [dbo].[LK_Estacion]    Script Date: 05/25/2015 15:42:39 ******/
+/****** Object:  Table [dbo].[LK_Estacion]    Script Date: 05/25/2015 15:57:11 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -10,15 +10,15 @@ CREATE TABLE [dbo].[LK_Estacion](
 	[ID_Estacion] [numeric](18, 0) NOT NULL,
 	[SK_Barrio] [numeric](18, 0) NOT NULL,
 	[Nombre] [nvarchar](255) NOT NULL,
-	[Latitud] [decimal](18, 0) NOT NULL,
-	[Longitud] [decimal](18, 0) NOT NULL,
+	[Latitud] [decimal](18, 10) NOT NULL,
+	[Longitud] [decimal](18, 10) NOT NULL,
  CONSTRAINT [PK_LK_Estacion] PRIMARY KEY CLUSTERED 
 (
 	[SK_Estacion] ASC
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[LK_Barrio]    Script Date: 05/25/2015 15:42:39 ******/
+/****** Object:  Table [dbo].[LK_Barrio]    Script Date: 05/25/2015 15:57:11 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -32,7 +32,7 @@ CREATE TABLE [dbo].[LK_Barrio](
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[LK_Provincia]    Script Date: 05/25/2015 15:42:39 ******/
+/****** Object:  Table [dbo].[LK_Provincia]    Script Date: 05/25/2015 15:57:11 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -46,7 +46,7 @@ CREATE TABLE [dbo].[LK_Provincia](
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[LK_Localidad]    Script Date: 05/25/2015 15:42:39 ******/
+/****** Object:  Table [dbo].[LK_Localidad]    Script Date: 05/25/2015 15:57:11 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -61,7 +61,7 @@ CREATE TABLE [dbo].[LK_Localidad](
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[LK_Usuario]    Script Date: 05/25/2015 15:42:39 ******/
+/****** Object:  Table [dbo].[LK_Usuario]    Script Date: 05/25/2015 15:57:11 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -79,13 +79,13 @@ CREATE TABLE [dbo].[LK_Usuario](
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  ForeignKey [FK_LK_Localidad_LK_Provincia]    Script Date: 05/25/2015 15:42:39 ******/
+/****** Object:  ForeignKey [FK_LK_Localidad_LK_Provincia]    Script Date: 05/25/2015 15:57:11 ******/
 ALTER TABLE [dbo].[LK_Localidad]  WITH NOCHECK ADD  CONSTRAINT [FK_LK_Localidad_LK_Provincia] FOREIGN KEY([SK_Provincia])
 REFERENCES [dbo].[LK_Provincia] ([SK_Provincia])
 GO
 ALTER TABLE [dbo].[LK_Localidad] CHECK CONSTRAINT [FK_LK_Localidad_LK_Provincia]
 GO
-/****** Object:  ForeignKey [FK_LK_Usuario_LK_Localidad]    Script Date: 05/25/2015 15:42:39 ******/
+/****** Object:  ForeignKey [FK_LK_Usuario_LK_Localidad]    Script Date: 05/25/2015 15:57:11 ******/
 ALTER TABLE [dbo].[LK_Usuario]  WITH CHECK ADD  CONSTRAINT [FK_LK_Usuario_LK_Localidad] FOREIGN KEY([SK_Localidad])
 REFERENCES [dbo].[LK_Localidad] ([SK_Localidad])
 GO
