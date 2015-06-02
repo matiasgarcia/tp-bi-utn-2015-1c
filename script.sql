503bc89e5b06586208ce0d21036ebab0ebb96fe8
Matias Garcia <mati92.ss@gmail.com>
2015-06-01 22:43:06 -0300

    Add geolocated TMIs

--- script.sql
+++ script.sql
@@ -305,6 +305,12 @@ CREATE TABLE [dbo].[LK_Usuario](
 ) ON [PRIMARY]
 END
 GO
+CREATE TABLE [dbo].[AUX_TMI_Geolocalizadas](
+	[ID_Estacion] [numeric](18, 0) NOT NULL,
+	[Latitud] [decimal](18, 10) NOT NULL,
+	[Longitud] [decimal](18, 10) NOT NULL
+) ON [PRIMARY]
+GO
 IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_AUX_TMI_LK_Barrio]') AND parent_object_id = OBJECT_ID(N'[dbo].[AUX_TMI]'))
 ALTER TABLE [dbo].[AUX_TMI]  WITH CHECK ADD  CONSTRAINT [FK_AUX_TMI_LK_Barrio] FOREIGN KEY([SK_Barrio])
 REFERENCES [dbo].[LK_Barrio] ([SK_Barrio])
@@ -325,4 +331,26 @@ REFERENCES [dbo].[LK_Localidad] ([SK_Localidad])
 GO
 IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_LK_Usuario_LK_Localidad]') AND parent_object_id = OBJECT_ID(N'[dbo].[LK_Usuario]'))
 ALTER TABLE [dbo].[LK_Usuario] CHECK CONSTRAINT [FK_LK_Usuario_LK_Localidad]
+GO
+INSERT INTO [dbo].[AUX_TMI_Geolocalizadas]
+           ([ID_Estacion]
+           ,[Latitud]
+           ,[Longitud])
+     VALUES
+           (1282,-34.6598512,-58.4819119),
+           (1283,-34.639466,-58.5212123),
+           (1284,-34.6282492,-58.4895115),
+           (1286,-34.6445735,-58.417351),
+           (1289,-34.562114,-58.457026),
+           (1291,-34.6357128,-58.397782),
+           (1292,-34.8685441,-58.7120687),
+           (1296,-34.6036768,-58.5082866),
+           (1295,-34.5656578,-58.4824158),
+           (1305,-34.6114069,-58.4208386),
+           (1304,-34.6043863,-58.4070812),
+           (1313,-34.5884245,-58.4110323),
+           (1314,-34.5957487,-58.3926404),
+           (1315,-34.6195681,-58.4358213),
+           (1316,-34.6315655,-58.4692706),
+           (1318,-34.600072,-58.378335)
 GO
