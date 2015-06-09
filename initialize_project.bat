91f1339ffc9710091ab930df649b8d87c5c5c677
Matias Garcia <mati92.ss@gmail.com>
2015-06-09 01:15:17 -0300

    Copy Datos_Usuarios.xsd

--- initialize_project.bat
+++ initialize_project.bat
@@ -1,5 +1,6 @@
-REM muevo INPUTs a un directorio común
+﻿REM muevo INPUTs a un directorio común
 echo D | xcopy INPUTs C:\BI\INPUTs /S /E
+copy Datos_Usuarios.xsd C:\BI\Ascii
 
 REM cargo scripts SQL
 sqlcmd -S localhost -i crear_base_de_datos.sql
